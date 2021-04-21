e299f7e7aed1591b751fa5cd5e162064f17ce6df
ricardo-faria <ricardo.jonas.faria@gmail.com>
2021-04-21 12:39:05 -0300

    committing code from lesson 1 and 2 of NWL 5

---
+++ src/repositories/SettingsRepository.ts
@@ -0,0 +1,10 @@
+import { Repository, EntityRepository } from "typeorm";
+import { Settings } from "../entities/Settings";
+
+@EntityRepository(Settings)
+class SettingsRepository extends Repository<Settings> {
+
+}
+
+
+export { SettingsRepository };
